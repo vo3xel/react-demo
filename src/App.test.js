604c3cd7f7fe21be34a2738902b9a31648e4f052
Michael Spitzer <vo3xel@gmail.com>
2020-02-12 17:13:05 +0100

    test added

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import App from './App'
+import { Provider } from 'react-redux'
+import configureStore from 'redux-mock-store'
+
+const initialState = { auth: { }, user: {}}
+const mockStore = configureStore()
+let store,wrapper
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />)
-  const linkElement = getByText(/get user details/i)
-  expect(linkElement).toBeInTheDocument()
+  store = mockStore(initialState)
+  const { getByText } = render(<Provider store={store}><App /></Provider>)
+  const element = getByText(/react application with react 16/i)
+  expect(element).toBeInTheDocument()
 })
--- src/components/UserDetails/UserDetails.test.js
+++ src/components/UserDetails/UserDetails.test.js
@@ -1,3 +1,17 @@
 import React from 'react'
 import { render } from '@testing-library/react'
-import App from './App'
+import { Provider } from 'react-redux'
+import configureStore from 'redux-mock-store'
+import { UserDetails } from '.'
+
+const initialState = { auth: { }, user: {}}
+const mockStore = configureStore()
+let store
+
+test('renders correctly', () => {
+    store = mockStore(initialState)
+    const { getByText, getByRole, container, asFragment } = render(
+        <Provider store={store}><UserDetails /></Provider>
+        )
+  expect(container).toMatchInlineSnapshot();
+})
