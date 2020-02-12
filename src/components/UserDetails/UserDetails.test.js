96df746eb830585631a359dd9a94778c1cca650b
Michael Spitzer <vo3xel@gmail.com>
2020-02-12 17:15:15 +0100

    tests added

--- src/components/UserDetails/UserDetails.test.js
+++ src/components/UserDetails/UserDetails.test.js
@@ -1,17 +1,26 @@
-import React from 'react'
-import { render } from '@testing-library/react'
-import { Provider } from 'react-redux'
-import configureStore from 'redux-mock-store'
-import { UserDetails } from '.'
+import React from "react";
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+import { UserDetails } from ".";
 
-const initialState = { auth: { }, user: {}}
-const mockStore = configureStore()
-let store
+const initialState = { auth: {}, user: {} };
+const mockStore = configureStore();
+let store;
 
-test('renders correctly', () => {
-    store = mockStore(initialState)
-    const { getByText, getByRole, container, asFragment } = render(
-        <Provider store={store}><UserDetails /></Provider>
-        )
-  expect(container).toMatchInlineSnapshot();
-})
+test("renders correctly", () => {
+  store = mockStore(initialState);
+  const { getByText, getByRole, container, asFragment } = render(
+    <Provider store={store}>
+      <UserDetails />
+    </Provider>
+  );
+  expect(container).toMatchInlineSnapshot(`
+    <div>
+      <div>
+        User details for user (
+        ):
+      </div>
+    </div>
+  `);
+});
