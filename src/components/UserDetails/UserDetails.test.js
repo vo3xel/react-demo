e0730fddbbde782ee58606ec88918f2fcf286fdf
Michael Spitzer <vo3xel@gmail.com>
2020-02-13 11:02:46 +0100

    more test cases added

--- src/components/UserDetails/UserDetails.test.js
+++ src/components/UserDetails/UserDetails.test.js
@@ -5,10 +5,24 @@ import configureStore from "redux-mock-store";
 import { UserDetails } from ".";
 
 const initialState = { auth: {}, user: {} };
+const userTestState = {
+  auth: { userId: 1 },
+  user: {
+    user: {
+      id: 1,
+      userName: "vo3xel",
+      password: "123456",
+      email: "vo3xel@gmail.com",
+      firstName: "Michael",
+      lastName: "Spitzer"
+    }
+  }
+};
+
 const mockStore = configureStore();
 let store;
 
-test("renders correctly", () => {
+test("renders empty correctly", () => {
   store = mockStore(initialState);
   const { getByText, getByRole, container, asFragment } = render(
     <Provider store={store}>
@@ -20,6 +34,27 @@ test("renders correctly", () => {
       <div>
         User details for user (
         ):
+      </div>
+    </div>
+  `);
+});
+
+test("renders user data correctly", () => {
+  store = mockStore(userTestState);
+  const { getByText, getByRole, container, asFragment } = render(
+    <Provider store={store}>
+      <UserDetails />
+    </Provider>
+  );
+  expect(container).toMatchInlineSnapshot(`
+    <div>
+      <div>
+        User details for user (
+        1
+        ):
+        <div>
+          {"id":1,"userName":"vo3xel","password":"123456","email":"vo3xel@gmail.com","firstName":"Michael","lastName":"Spitzer"}
+        </div>
       </div>
     </div>
   `);
